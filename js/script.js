75c5e1cd8f08a6bbbd0dd634529568c1698eac58
Nick Berens <nberens@wisnet.com>
2012-08-22 09:12:17 -0500

    Reponsive nav first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* Author:
+
+*/
+jQuery(document).ready(function($){
+	
+	<!--jQuery('#nav').prepend('<div id="menu-icon">Menu</div>');-->
+
+	$('#menu-icon').click(function(){
+		$('nav').slideToggle();
+	});
+
+});
+
+
+
+
